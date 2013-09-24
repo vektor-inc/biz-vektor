5a2cee2c71b2ccd8c4750dd0b28a1c2e2354c457
Hidekazu Ishikawa <kurudrive@gmail.com>
2013-09-25 02:44:29 +0900

    多言語化追加
    mainfoot.php関連、問い合わせボタン

--- module_mainfoot.php
+++ module_mainfoot.php
@@ -1,4 +1,4 @@
-<?php // 電話番号または問い合わせページのURLが設定されている場合に表示
+<?php
 $options = biz_vektor_get_theme_options();
 if ($options['contact_link'] || $options['tel_number']) :?>
 <div class="mainFoot">
@@ -11,7 +11,9 @@ if ($options['contact_link'] || $options['tel_number']) :?>
 <?php // 問い合わせページのURLが設定されている場合に表示
 $options = biz_vektor_get_theme_options();
 if ($options['contact_link']) :?>
-<div class="mainFootBt"><a href="<?php echo $options['contact_link'] ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/bt_contact.png" alt="メールでお問い合わせはこちら" /></a></div> 
+<div class="mainFootBt"><a href="<?php echo $options['contact_link'] ?>">
+	<img src="<?php echo get_template_directory_uri(); ?>/images/<?php _e('bt_contact.png', 'biz-vektor'); ?>" alt="<?php _e('Contact us by e-mail', 'biz-vektor'); ?>" /></a>
+</div> 
 <?php endif; ?>
 </div> 
 <!-- [ /.mainFootContact ] -->
