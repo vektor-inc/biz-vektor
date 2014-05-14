05832a2f421b2ac0c51050e838a7546b4d15edfe
Vektor,Inc <info@vektor-inc.co.jp>
2014-05-14 11:23:52 +0900

    投稿コンテンツでサイドバーウィジェットに何も設定していない場合、問い合わせボタンが二重で出力される不具合を修正

--- module_side_post.php
+++ module_side_post.php
@@ -13,16 +13,7 @@ else :
 	</ul>
 	</div>
 	</div>
-	<?php 
-	$options = biz_vektor_get_theme_options();
-	if ($options['contact_link']) :?>
-	<div class="sideWidget">
-	<ul>
-	<li class="sideBnr" id="sideContact"><a href="<?php echo $options['contact_link'] ?>">
-	<img src="<?php echo get_template_directory_uri(); ?>/images/<?php _e('bnr_contact.png', 'biz-vektor'); ?>" alt="<?php _e('Contact us by e-mail', 'biz-vektor'); ?>"></a></li>
-	</ul>
-	</div>
-	<?php endif;
+	<?php
 	if (function_exists('biz_vektor_contactBtn')) biz_vektor_contactBtn();
 	if (function_exists('biz_vektor_snsBnrs')) biz_vektor_snsBnrs();
 	if (function_exists('biz_vektor_fbLikeBoxSide')) biz_vektor_fbLikeBoxSide();
