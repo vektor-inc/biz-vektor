748e66c4f0f80a49274d6e7be237fd93b73bd702
緋村浅緋 <d16gp13@gmail.com>
2014-07-11 19:02:33 +0900

    Revert "Revert "Revert "軽度のPHP警告修正"""
    This reverts commit 048321dec3f9e872666309b0e043261f66a0d1ce.

--- functions.php
+++ functions.php
@@ -359,8 +359,9 @@ function getHeadDescription() {
 	global $wp_query;
 	$post = $wp_query->get_queried_object();
 	if (is_home() || is_page('home') || is_front_page()) {
-		if ( isset($post->post_excerpt) ) {
-			$metadescription = $post->post_excerpt;
+		$metaExcerpt = $post->post_excerpt;
+		if ( $metaExcerpt ) {
+			$metadescription = $metaExcerpt;
 		} else {
 			$metadescription = get_bloginfo( 'description' );
 		}
