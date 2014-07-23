9e3276cd393ad157cd6a200ad3afd79d8abf43e5
Vektor,Inc <info@vektor-inc.co.jp>
2014-07-23 19:27:56 +0900

    ポストタイプ名が正しく取れない現象があったため修正

--- module_panList.php
+++ module_panList.php
@@ -6,9 +6,9 @@ global $wp_query;
 // カスタム投稿タイプの種類を取得
 $postType = get_post_type();
 // カスタム投稿タイプ名を取得
-if(isset($postType->labels->name)){
-	$postTypeName = esc_html(get_post_type_object($postType->labels->name));
-}
+$post_type_object = get_post_type_object($postType);
+$postTypeName = esc_html($post_type_object->labels->name);
+
 // 標準のpost のラベル名
 $postLabelName = bizVektorOptions('postLabelName');
 	echo '<ul>';
