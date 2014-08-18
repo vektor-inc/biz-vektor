170a755ef243676dc0f0966903f4643f8d01ce13
Vektor,Inc <info@vektor-inc.co.jp>
2014-08-18 13:09:57 +0900

    サムネイル有りの一覧の場合の仕様変更（画像のサイズに応じてキレイに追従するようにres-vektor使用）

--- module_loop_post2.php
+++ module_loop_post2.php
@@ -13,8 +13,8 @@ if ($postType == 'post') {
 $taxo_catelist = get_the_term_list( $post->ID, $taxonomySlug, ' ','','');
 ?>
 <!-- [ .infoListBox ] -->
-<div class="infoListBox">
-	<div class="entryTxtBox <?php if ( has_post_thumbnail()) : ?>haveThumbnail<?php endif; ?>">
+<div class="infoListBox ttBox">
+	<div class="entryTxtBox ttBoxTxt<?php if ( has_post_thumbnail()) echo ' haveThumbnail'; ?>">
 	<h4 class="entryTitle">
 	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 	<?php if ( is_user_logged_in() == TRUE ) : edit_post_link(__('Edit', 'biz-vektor'), '<span class="edit-link edit-item">[ ', ' ]</span>');endif ?>
@@ -27,7 +27,7 @@ $taxo_catelist = get_the_term_list( $post->ID, $taxonomySlug, ' ','','');
 	</div><!-- [ /.entryTxtBox ] -->
 	
 	<?php if ( has_post_thumbnail()) { ?>
-		<div class="thumbImage">
+		<div class="thumbImage ttBoxThumb">
 		<div class="thumbImageInner">
 		<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
 		</div>
