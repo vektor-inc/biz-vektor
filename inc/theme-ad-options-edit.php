14a16074e52404922e21980c22ae1446b5713aa4
Dorothee SEGARD <dorothee.segard@gmail.com>
2014-10-22 18:14:51 +0900

    「高度な設定」管理画面からサイトマップページに追加する投稿タイプと除外する固定ページを入力するようになりました

--- inc/theme-ad-options-edit.php
+++ inc/theme-ad-options-edit.php
@@ -1,29 +1,36 @@
 <div class="wrap biz_vektor_options">
 
-	<h1><?php _e('高度な設定', 'biz-vektor') ?></h1>
+	<h2><?php echo get_biz_vektor_name(); ?> <?php _e('高度な設定', 'biz-vektor') ?></h2>
+
+	<?php echo $data['mess'] ?>
+	<div class="fileedit-sub"></div>
+
 	<div class="message_intro">
-		高度な設定の説明
+		<p>
+			高度な設定の説明
+		</p>
 	</div>
 
-	<?php //echo $data['mess'] ?>
-
 	<form method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
+		<?php wp_nonce_field( 'submit-sitemap', 'nonce-sitemap' ); ?>
 
-		<div class="sectionBox">
-			
+		<!-- [SITEMAP POST TYPES] -->
+		<div class="sectionBox" id="post-type">
+
 			<h3><?php _e('サイトマップに追加する投稿タイプ', 'biz-vektor') ?></h3>
+
 			<table class="form-table">
 				<tbody>
 					<tr>
-						<th><?php _e('投稿タイプ', 'biz-vektor') ?></th>
+						<th>
+							<label for="types">
+								<?php _e('投稿タイプ', 'biz-vektor') ?>
+							</label>
+						</th>
 						<td>
 							<p>
-								<?php 
-									//var_dump(get_pages()); 
-								?>
-								<?php wp_list_pages('title_li=') ?>
-								<input type="checkbox" id="" name="" />
-								<label for=""></label>
+								<input type="text" id="types" name="types" placeholder="service,product" value="<?php echo $data['types'] ?>" />
+								&nbsp;※追加したい投稿タイプをコンマで区切って入力してください。
 							</p>
 						</td>
 					</tr>
@@ -33,17 +40,24 @@
 				<input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('変更を保存', 'biz-vektor') ?>" />
 			</p>
 		</div><!-- /.sectionBox -->
+		<!-- [/SITEMAP POST TYPES] -->
 
-		<div class="sectionBox">
+
+		<!-- [SITEMAP PAGES] -->
+		<div class="sectionBox" id="page">
+
 			<h3><?php _e('サイトマップから除外する固定ページ', 'biz-vektor') ?></h3>
-					<table class="form-table">
+
+			<table class="form-table">
 				<tbody>
 					<tr>
-						<th><?php _e('Pages') ?></th>
+						<th>
+							<label for="pages"><?php _e('Pages') ?></label>
+						</th>
 						<td>
 							<p>
-								<input type="checkbox" id="" name="" />
-								<label for=""></label>
+								<input type="text" id="pages" name="pages" placeholder="35,1654" value="<?php echo $data['pages'] ?>" />
+								&nbsp;※除外したい固定ページのidをコンマで区切って入力してください。
 							</p>
 						</td>
 					</tr>
@@ -53,6 +67,8 @@
 				<input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('変更を保存', 'biz-vektor') ?>" />
 			</p>
 		</div><!-- /.sectionBox -->
+		<!-- [/SITEMAP PAGES] -->
+
 	</form><!-- end from-->
 
 
--- inc/theme-ad-options.php
+++ inc/theme-ad-options.php
@@ -10,7 +10,7 @@ class Biz_Vektor_Advanced_Options {
 		add_theme_page( 
 			__('高度な設定', 'biz-vektor'),
 			__('高度な設定', 'biz-vektor'),
-			'manage_options',
+			'edit_theme_options',
 			'theme_advanced_options',
 			array('Biz_Vektor_Advanced_Options', 'displayView') 
 		);
@@ -19,10 +19,28 @@ class Biz_Vektor_Advanced_Options {
 	}
 
 	public static function displayView() {
-	
+
+		//prepares data to display inside view
+		$data = array(
+			'types' => '',
+			'pages' => '',
+			'mess' 	=> ''
+		);
+
+		//verif form + saves new data
+		$data['mess'] 	= self::verifForm();
+
+		//gets advanced options saved in DB
+		$adOptions 		= self::getAdvancedOptions();
+
+		$data['types']	= !empty($adOptions['types']) ? implode( ',', $adOptions['types'] ) : '';
+		$data['pages']	= !empty($adOptions['pages']) ? implode( ',', $adOptions['pages'] ) : '';
+
+
 		require( 'theme-ad-options-edit.php' );
 	}
 
+	//load Biz Vektor admin css
 	public static function loadCss() {
 
 		Global $hook_suffix;
@@ -32,6 +50,72 @@ class Biz_Vektor_Advanced_Options {
 			wp_enqueue_style( 'style_bizvektor_admin' );
 
 		}
-			require( 'theme-ad-options.css' );
+	}
+
+	//form validation
+	private static function verifForm() {
+		
+		$mess = '';
+
+		if ( isset($_POST['submit']) && !empty($_POST['submit'])
+			 && ( isset($_POST['types']) || isset($_POST['pages']) )
+			 && wp_verify_nonce( $_POST['nonce-sitemap'], 'submit-sitemap' ) ) {
+
+			if ( self::updateAdvancedOptions( $_POST['types'], $_POST['pages'] ) )
+				$mess = '<div id="message" class="updated"><p>' . __( '設定が保存されました。', 'biz-vektor') . '</p></div>';	
+		}
+		else {
+			if ( isset($_POST['submit']) && !empty($_POST['submit']) )
+				$mess = '<div id="message" class="error"><p>' . __( 'エラーが発生しました。', 'biz-vektor') . '</p></div>';
+		}
+
+		return $mess;
+	}
+
+	//manages update of values
+	private static function updateAdvancedOptions( $types, $pages ) {
+
+		//cleaning values before saving to DB
+		$toclean = array(
+			'types' => $types,
+			'pages' => $pages
+		);
+
+		$cleaned = array(
+			'types' => array(),
+			'pages' => array(),
+		);
+
+		foreach ( $toclean as $key => $value ) {
+			
+			$valuesArray 	= explode( ',', $value );
+			$cleanedValues 	= array();
+
+			foreach ( $valuesArray as $val ) {
+
+				if ( $val != '' ) {
+
+					$cleanValue = '';
+
+					if( $key == 'pages' && !empty($val) ) 
+						$cleanValue = strip_tags(trim(intval($val)));	
+					else
+						$cleanValue = strip_tags(trim($val));
+
+					$cleanedValues[] = $cleanValue;
+				}
+ 			}
+
+			$cleaned[$key] = $cleanedValues;	
+		}
+
+		//saving to DB
+		return update_option('biz_vektor_ad_options', $cleaned);
+	}
+
+	public static function getAdvancedOptions() {
+
+		$res = get_option('biz_vektor_ad_options');
+		return !$res ? '' : $res; 
 	}
 }
