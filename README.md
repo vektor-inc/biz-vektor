c1196dcb9ed2e94c6380b60882a8a3c32ae2049b
kurudrive <kurudrive@gmail.com>
2014-10-27 21:46:00 +0900

    バージョン変更

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Vektor,Inc.
 ## Author URI
 http://www.vektor-inc.co.jp/
 ## Version
-1.3.2
+1.3.3
 ## License
 GNU General Public License
 
--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BizVektor_Theme_Version', '1.3.2');
+define('BizVektor_Theme_Version', '1.3.3');
 
 /*-------------------------------------------*/
 /*	Set content width
