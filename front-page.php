4a59658639df4b738fafa24ba6ee479d6b1a504c
Shoji Imamura <imamura@vektor-inc.co.jp>
2015-01-13 11:56:11 +0900

    front-pageの文言を復活

--- front-page.php
+++ front-page.php
@@ -40,6 +40,29 @@ else :
 	<?php endif; // $topFreeContent ?>
 	<?php endif; // get_post_type() === 'page' ?>
 
+	<?php if ( is_user_logged_in() == TRUE ) {
+	global $user_level;
+	get_currentuserinfo(); ?>
+		<div class="adminEdit">
+			<?php if (10 <= $user_level) { ?>
+			<p class="caption">
+			<?php _e('* In admin [Settings] &raquo; [Display Settings], if the front page is not set to a [page], nothing is displayed in this area.', 'biz-vektor'); ?><br />
+			<?php _e('* If empty, the body of a page that you set as the front page does not display anything.', 'biz-vektor'); ?><br />
+			<?php // _e('* If you have set a specific page as the front page, pagination does not appear at the bottom.', 'biz-vektor'); ?>
+			</p>
+			<?php } ?>
+			<span class="linkBtn linkBtnS linkBtnAdmin" style="float:left;margin-right:10px;"><?php edit_post_link( __('Edit', 'biz-vektor') ); ?></span>
+			<?php if (10 <= $user_level) { ?>
+			<span style="float:left;margin-right:10px;"><a href="<?php echo site_url(); ?>/wp-admin/themes.php?page=theme_options#topPage" class="btn btnS btnAdmin">
+				<?php _e('Title display settings', 'biz-vektor'); ?>
+			</a></span>
+			<span><a href="<?php echo site_url(); ?>/wp-admin/options-reading.php" class="btn btnS btnAdmin">
+				<?php _e('Change the page to be displayed', 'biz-vektor'); ?>
+			</a></span>
+			<?php } ?>
+		</div>
+	<?php } // login ?>
+	
 	<?php endif; // have_posts() ?>
 
 	<?php get_template_part('module_topPR'); ?>
