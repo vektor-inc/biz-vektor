f3bcfab21abaf77a5aaa8cac14a0d568a9bd6f07
Shoji Imamura <imamura@vektor-inc.co.jp>
2015-06-12 13:45:23 +0900

    jsのエラー対策

--- js/master.js
+++ js/master.js
@@ -153,6 +153,7 @@ jQuery(document).ready(function(){
 	jQuery('a[href*=#]').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 			var $target = jQuery(this.hash);
+			if(!this.hash.slice(1)){return;}
 			$target = $target.length && $target || jQuery('[name=' + this.hash.slice(1) +']');
 			if ($target.length) {
 				var targetOffset = $target.offset().top;
