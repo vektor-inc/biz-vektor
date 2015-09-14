e4186032a70ee622fec08636d9c76e988d12cfe2
shoji imamura <imamura@vektor-inc.co.jp>
2015-09-14 15:41:29 +0900

    修正

--- header.php
+++ header.php
@@ -10,7 +10,7 @@ biz_vektor_get_theme_options(); ?>
 <title><?php wp_title(); ?></title>
 <meta name="description" content="<?php getHeadDescription(); ?>" />
 <link rel="start" href="<?php echo home_url(); ?>" title="HOME" />
-<link rel="alternate" href="<?php echo home_url(); ?>" hreflang="<?php echo get_bloginfo('language'); ?>" />
+<link rel="alternate" href="<?php echo home_url(); ?>" hreflang="<?php echo substr(get_bloginfo ( 'language' ), 0, 2);?>" />
 <!-- <?php echo get_biz_vektor_name();?> v<?php echo BizVektor_Theme_Version; ?> -->
 
 <?php
