f0ff11bef0db60b6d3db0a235b9c6b21bee9c82c
shoji imamura <imamura@vektor-inc.co.jp>
2016-02-25 17:46:17 +0900

    メッセージの追加

--- deprecations.php
+++ deprecations.php
@@ -1,5 +1,8 @@
 <?php
 
 if( !function_exists('biz_vektor_getHeadKeywords') ){
-    function biz_vektor_getHeadKeywords(){}
+    function biz_vektor_getHeadKeywords(){
+        trigger_error("biz_vektor_getHeadKeywords()は非推奨となりました。子ページテンプレートのheader.phpより該当部分を削除してください。(キーワードは別の方法でちゃんと出力されています!)");
+        return '';
+    }
 }
