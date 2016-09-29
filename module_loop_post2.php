f4ac639c2a6cfa51cbd69641aef31f42d6f126d4
kurudrive <kurudrive@gmail.com>
2016-09-29 09:04:13 +0900

    [ フィルターフック追加 ] module_loop_post2 と 3PRの 詳しくはこちら ボタンの文言をフックで変更しやすいようにフック追加

--- module_loop_post2.php
+++ module_loop_post2.php
@@ -23,7 +23,7 @@ $taxo_catelist = get_the_term_list( $post->ID, $taxonomySlug, ' ','','');
 	<span class="infoDate"><?php echo esc_html( get_the_date() ); ?></span><span class="infoCate"><?php echo $taxo_catelist; ?></span>
 	</p>
 	<p><?php the_excerpt(); ?></p>
-	<div class="moreLink"><a href="<?php the_permalink(); ?>"><?php _e('Read more', 'biz-vektor'); ?></a></div>
+	<div class="moreLink"><a href="<?php the_permalink(); ?>"><?php echo esc_html( apply_filters( 'read_more_txt', __( 'Read more', 'biz-vektor' ) ) ); ?></a></div>
 	</div><!-- [ /.entryTxtBox ] -->
 	
 	<?php if ( has_post_thumbnail()) { ?>
